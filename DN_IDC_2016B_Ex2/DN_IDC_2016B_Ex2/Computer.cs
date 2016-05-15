8877fea7fa634082679683024dab0bfd9f041311
galbh2 <galbh2@gmail.com>
2016-05-15 11:01:59 -0700

    added Computer class

---
+++ DN_IDC_2016B_Ex2/DN_IDC_2016B_Ex2/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN_IDC_2016B_Ex2
+{
+    class Computer : Player
+    {
+        public Computer(string i_Name, bool i_IsComputer, char i_Symbol, Board i_Board) : base(i_Name, i_IsComputer, i_Symbol, i_Board)
+        {
+        }
+
+
+        public override eGameStatus Insert(int i_Col)
+        {
+            Random random = new Random();
+            int col = random.Next(0, m_Board.getBoard().GetLength(1));
+            return m_Board.insert(col, m_Symbol);
+        }
+    }
+}
