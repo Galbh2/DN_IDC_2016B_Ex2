6b80a144265b2a4213fbd6c685435212570bacd7
galbh2 <galbh2@gmail.com>
2016-05-14 18:33:01 -0700

    added data verification to Cmd

--- DN_IDC_2016B_Ex2/DN_IDC_2016B_Ex2/Cmd.cs
+++ DN_IDC_2016B_Ex2/DN_IDC_2016B_Ex2/Cmd.cs
@@ -17,6 +17,8 @@ namespace DN_IDC_2016B_Ex2
         private string m_WinMsg = "Player {0} Won !";
         private string m_TieMsg = "It's a Tie !";
         private string m_PlayAgainMsg = "Do you want to play again ?";
+        private string m_PlayerAName = "Player a";
+        private string m_PlayerBName = "Player b";
 
         private readonly int m_MinNum = 4;
         private readonly int m_MaxNum = 8;
@@ -38,13 +40,72 @@ namespace DN_IDC_2016B_Ex2
 
         private void startFirstGame()
         {
-            int numOfRows, numOfCols;
-            bool computerMode;
+            int numOfRows = 1;
+            int numOfCols;
+            int computerMode;
+            string playerBName =
 
-            numOfRows = getNumFromUser(m_AskForRows);
+                        // Getting the input for initializing the game 
+            getNumFromUser(m_AskForRows);
+            checkExitCondition(numOfRows);
             numOfCols = getNumFromUser(m_AskForCols);
+            checkExitCondition(numOfCols);
+            computerMode = getComputerMode();
+            checkExitCondition(computerMode);
+            parseComputerMode(computerMode);
+            
+            if (m_ComputerMode)
+            {
+                m_PlayerBName = "Computer";
+            }
 
-      
+            m_GameManager = new GameManager(m_PlayerAName, m_PlayerBName,
+                                m_ComputerMode, numOfRows, numOfCols);
+
+        }
+
+        private void parseComputerMode(int i_ComputerMode)
+        {
+            bool playInComputerMode = true;
+
+            if (i_ComputerMode == 1)
+            {
+                m_ComputerMode = playInComputerMode;
+            } else
+            {
+                m_ComputerMode = !playInComputerMode;
+            }
+        }
+
+        private void checkExitCondition(int i_ExitCode)
+        {
+            if (i_ExitCode == -1)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private int getComputerMode()
+        {
+            bool succeedd = false;
+            int result = 1;
+            string input;
+
+            while (!succeedd)
+            {
+                System.Console.WriteLine(m_AskForNumOfPlayers);
+                input = System.Console.ReadLine();
+
+                if (input.Equals('Q'))
+                {
+                    return -1;
+                }
+                succeedd = int.TryParse(input, out result);
+                succeedd = succeedd && (result == 1 || result == 2);
+            }
+
+            return result;
+
         }
 
         private int getNumFromUser(string i_Msg)
